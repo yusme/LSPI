53b37b5883e3f9d0707cb5ef282586f95578775c
yusme <yeimy.valencia1@outlook.de>
2018-04-12 10:45:23 +0000

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from policy import Policy
 from rbf import Basis_Function
 from lspi import LSPI, LSTDQ
 import gym
-from scipy import *
+import scipy 
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -33,26 +33,10 @@ mean_reward1=[]
 mean_reward2=[]
 
 
-
-
-#TODO: initialize gym environment
-#TODO: Write a class to calculate MDp
-#TODO: write a class to get the optimal Policy
-
-#TODO: christian
-#todo:1 LSPI Model based (LSTD-Q)
-#todo:2 LSPI No sample reuse
-#todo:3 LSPI Classic is mixture policy
-#todo:4 LSPI state correct IS
-
-
-# this program for discrete space
-
 def test_policy(policy,env, state,agent):
+   
+    print ("Test")
 
-    print "============="
-    print "   Test      "
-    print "============="
     total_reward = 0.0
     state = env.reset()
 
@@ -65,7 +49,7 @@ def test_policy(policy,env, state,agent):
             index = policy.get_actions(state)  # TODO: valication for random actions
             #action = policy.actions[index[0]]  # todo. take just one action
             action=agent._act(state)
-            #print "action",action
+
 
             next_state, reward, done, info = env.step(action)
             state = next_state
@@ -104,27 +88,18 @@ def _initial_sample2(env, memory,agent ):
 
         state = env.reset()
 
-        print "============="
-        print "   Training   "
-        print "============="
-
         best_theta = False
-        print "EPISODE",best_theta
-
 
         for i in range(TRANSITION):
 
 
            # env.render()
-
-
             #action = agent._act(state)
             if best_reward >= total_reward and found==False:
                 action = env.action_space.sample()
-                #print "entro en uniform"
 
             else:
-                #print "training with best theta", best_reward, total_reward
+
                 agent=Best_agent
                 best_theta = True
                 action = agent._act(state)
@@ -132,9 +107,7 @@ def _initial_sample2(env, memory,agent ):
             next_state, reward, done, info = env.step(action)
 
             memory.add([state, action, reward, next_state, done])
-            #print "action", action
 
-            #total_reward+=gamma*reward
             state = next_state
 
 
@@ -145,8 +118,6 @@ def _initial_sample2(env, memory,agent ):
 
         if j>0:
 
-
-            print "error transition population=", TRANSITION, memory.containerSize
 
             if done:
                 sample = memory.select_sample(j)
@@ -159,25 +130,13 @@ def _initial_sample2(env, memory,agent ):
 
             total_reward, policy_test =test_policy(policy, env, state,agent)
 
-            print "Best Theta=",best_theta
-
-            print "BEST REWARD= ", best_reward, total_reward
-
-            print "best_reward < total_reward === ++++", best_reward < total_reward
 
             if best_reward < total_reward:
-
-                print "==== entro===+++++  best_reward < total_reward",  best_reward < total_reward,best_reward, total_reward
-
-
 
                 Best_agent=agent
                 best_reward=total_reward
                 total_reward=-4950.0
                 #found=True
-
-                print "entro+++++++++++++-------  best_reward", best_reward, total_reward
-
 
 
             print "TEST---",j
@@ -185,9 +144,6 @@ def _initial_sample2(env, memory,agent ):
             if best_theta:
                 memory.clear_memory()
 
-
-
-       # mean_reward1.append(total_reward)
 
 
 
@@ -201,7 +157,8 @@ def _reuse_sample2(env, memory, agent ):
     important_sampling = False
 
     for j in range(EPISODE):
-        print "episode", j, "/", EPISODE
+
+        print ("episode", j, "/", EPISODE)
 
         state = env.reset()
 
@@ -217,7 +174,7 @@ def _reuse_sample2(env, memory, agent ):
                 sample = memory.select_sample(BATCH_SIZE)  # [current_state, actions, rewards, next_state, done]
                 agent.train(sample, lspi_interation, important_sampling)
 
-            #action = agent._act(state)
+
             next_state, reward, done, info = env.step(action)
             memory.add([state, action, reward, next_state, done])
 
@@ -231,7 +188,7 @@ def _reuse_sample2(env, memory, agent ):
         sample = memory.select_sample(BATCH_SIZE)  # [current_state, actions, rewards, next_state, done]
         agent.train(sample, lspi_interation, important_sampling)
         #memory.clear_memory()
-        print memory.containerSize
+        print (memory.containerSize)
 
 
     return mean_reward2
@@ -249,7 +206,7 @@ def experiment_1():
 
     memory = Memory(MEMORY_SIZE, BATCH_SIZE,
                     action_dim,  obs_dim)
-    print num_actions,obs_dim
+    print (num_actions,obs_dim)
 
     agent =  LSPI(num_actions,obs_dim )
     return agent, env, memory
@@ -261,12 +218,12 @@ def experiment_2():
 
     action_dim = 1
     num_actions = env.action_space.n
-    obs_dim = env.observation_space.shape[0]
-    memory = Memory(MEMORY_SIZE, BATCH_SIZE,
+    obs_dim     = env.observation_space.shape[0]
+    memory      = Memory(MEMORY_SIZE, BATCH_SIZE,
                     action_dim,  obs_dim)
 
     agent =  LSPI(num_actions,obs_dim)
-    print "---",obs_dim
+
     return agent, env , memory
 
 
@@ -274,31 +231,17 @@ def experiment_2():
 
 def main():
     #agent, env, memory = experiment_1()
-
     agent, env, memory = experiment_2()
 
-    a={ 'name': 123,  'vorname': "valencia"
-    }
-    print a['vorname']
-
-    """ PlOT """
-
-
-    print "memory size", memory.containerSize
+    print ("memory size", memory.containerSize)
 
     y2 =_reuse_sample2(env, memory, agent)
-    print y2
-
-    #memory.clear_memory()
-    #print "memory size", memory.containerSize
-
     y1 = _initial_sample2(env, memory, agent)
-    print y1
 
     x  = np.arange(0, len(mean_reward1))
 
     np.reshape(mean_reward1,x.shape)
-    print x.shape, mean_reward1, mean_reward2, x
+    print (x.shape, mean_reward1, mean_reward2, x)
 
     import plot as pl
     pj = pl.Plot()
@@ -317,7 +260,7 @@ def _initial_sample(env, memory, policy=None):
 
     for j in range(EPISODE):
 
-        print "episode-", j, "/", EPISODE, j / float(EPISODE)
+        print ("episode-", j, "/", EPISODE, j / float(EPISODE))
 
         for i in range(TRANSITION):
             env.render()
@@ -325,11 +268,10 @@ def _initial_sample(env, memory, policy=None):
             if policy is None:
                 action = env.action_space.sample()
             else:
-
                 sample = memory.select_sample(BATCH_SIZE)  # [current_state, actions, rewards, next_state, done]
                 policy = lspi.train(sample, lspi_interation, impotant_samplig)
-                index = policy.get_actions(state)  # TODO: valication for random actions
-                action = policy.actions[index[0]]  # todo. take just one action
+                index  = policy.get_actions(state)  # TODO: for random actions
+                action = policy.actions[index[0]]
 
             next_state, reward, done, info = env.step(action)
             memory.add([state, action, reward, next_state, done])
@@ -369,7 +311,7 @@ def _reuse_sample(env, memory):
                 policy = lspi.train(sample, lspi_interation, important_sampling)
                 index = policy.get_actions(state)
                 action = policy.actions[index[0]]
-                # lspi.td_error( sample)
+
 
                 memory.clear_memory()
 
@@ -380,11 +322,9 @@ def _reuse_sample(env, memory):
             if done:
                 print ("done")
                 state = env.reset()
-                # print "----", policy.weights, reward
-                # R = test_policy(policy, env,state)
                 break
 
-            mean_reward.append(total_reward)
+            mean_reward1.append(total_reward)
             total_reward = 0.0
 
 
